6800cbf4be97a7a70d612db0e9f4de5681ab20c0
Ostryl <ostryl@proton.me>
2025-12-30 16:17:11 -0800

    This is no good, I'm going to compress this into a single script. But saving this for __file__ reference.

--- fittocanvas.py
+++ fittocanvas.py
@@ -1,14 +1,22 @@
-from krita import *
 
+import sys
+from krita import *
+from imagefilter import *
+from pathlib import Path
+
+currentDir = Path(__file__).parent.resolve()
+
+if str(currentDir) not in sys.path:
+    sys.path.append(str(currentDir))
+
+application = Krita.instance()
+currentDoc = application.activeDocument()
+activeLayer = currentDoc.activeNode()
 
 def musicFilter():
-    application = Krita.instance()
-    currentDoc = application.activeDocument()
     if not currentDoc:
         print("No active document!")
         return
-
-    activeLayer = currentDoc.activeNode()
     if not activeLayer or activeLayer.type() != "paintlayer":
         print("Select a paint layer first!")
         return
@@ -27,4 +35,13 @@ def musicFilter():
     activeLayer.move(movementX,movementY)
     currentDoc.activeNode()
     currentDoc.refreshProjection()
+
+def copyLayer():
+    duplicateLayer = activeLayer.duplicate()
+    parentLayer = activeLayer.parentNode()
+    parentLayer.addChildNode(duplicateLayer, activeLayer)
+
+
 musicFilter()
+copyLayer()
+filterLayer()
---
+++ imagefilter.py
@@ -0,0 +1,8 @@
+
+from krita import *
+
+def filterLayer():
+    print("joy to the world")
+    
+
+
